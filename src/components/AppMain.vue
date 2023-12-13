57340ebd5c35d9df3d61298dd3967b41b8a7296a
AlbertoMessineo <albevorox@gmail.com>
2023-12-13 22:33:56 +0100

    feat: options added

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,12 @@
         name: 'AppMain',
         components: {
             CardsCollection,
-        }
+        },
+        data(){
+            return{
+                selectKey: '',
+            };
+        },
     };
 </script>
 
@@ -16,9 +21,18 @@
     <!-- contenitore carte -->
     <div class="container main p-4">
         <div class="col-2 mb-4 pt-3">
-            <select id="inputState" class="form-select">
-                <option selected>Alien</option>
-                <option>...</option>
+            <select id="inputState" class="form-select" v-model="selectKey">
+                <option selected value="Seleziona Archetipo">Seleziona archetipo</option>
+                <option value="Alien">Alien</option>
+                <option value="Infernoble Arms">Infernoble Arms</option>
+                <option value="Noble Knight">Noble Knight</option>
+                <option value="Melodious">Melodious</option>
+                <option value="Archfiend">Archfiend</option>
+                <option value="Umi">Umi</option>
+                <option value="Elemental HERO">Elemental HERO</option>
+                <option value="ABC">ABC</option>
+                <option value="A.I.">A.I.</option>
+                <option value="@Ignister">@Ignister</option>
             </select>
         </div>
         <div class="container found p-4">
